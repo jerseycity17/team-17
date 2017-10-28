cb8fc1700d54b64772e7fd67b8d9e933ff6a68ae
Samuel <Samuellin1996@gmail.com>
2017-10-28 00:55:46 -0400

    added database, bootstrap, bcrypt, models, view

--- models/users.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Users = sequelize.define('Users', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  });
-  return Users;
-};
